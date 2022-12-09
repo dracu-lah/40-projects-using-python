a51b405b1f5639f739b2ed251378b76c523861cd
dracu-lah <nevilnics4321@gmail.com>
2022-12-09 19:49:17 +0530

    created index of max values

---
+++ 33.index-of-maximum-values/main.py
@@ -0,0 +1,12 @@
+def maximum(x):
+    max_index = 0
+    current_index = 1
+    while current_index < len(x):
+        if x[current_index] > x[max_index]:
+            max_index = current_index
+        current_index += 1
+    return max_index
+
+a = [23, 76, 45, 20, 70, 65, 15, 54]
+
+print(maximum(a))
